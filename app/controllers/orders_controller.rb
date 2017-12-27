841bba7e914c92ac18a9557dad80b207127bedaa
KonstantinFaleev <konstantin.faleev@gmail.com>
2017-12-27 18:59:46 +0200

    errors controller order, model product

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,7 @@ class OrdersController < ApplicationController
     respond_to do |format|
       if @order.save
         Cart.destroy(session[:cart_id])
-        OrderNotifier.received(@order).deliver#_later
+        OrderNotifierMailer.received(@order).deliver_later
         session[:cart_id] = nil
         format.html { redirect_to store_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
